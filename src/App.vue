a1102b3429a41ebb81ff7c79904751b90706932c
evasyukov <egor.vasyukov1@gmail.com>
2024-08-21 14:49:01 +0700

    - добавлен блок с просмотренными фильмами - добавлена логика переключения таба

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,34 @@
       <img class="header-logo" src="" alt="" />
       <h2>My Favorite Movies</h2>
     </div>
-    <div class="movies">
+
+    <div class="tabs">
+      <button
+        class="btn"
+        :class="{ btn__green: movieStore.activeTab === 1 }"
+        @click="movieStore.setActiveTab(1)"
+      >
+        Favorite
+      </button>
+      <button
+        class="btn"
+        :class="{ btn__green: movieStore.activeTab === 0 }"
+        @click="movieStore.setActiveTab(0)"
+      >
+        Search
+      </button>
+    </div>
+
+    <div class="movies" v-if="movieStore.activeTab === 1">
+      <div>
+        <h3>Movies</h3>
+        <Movie
+          v-for="movie of movieStore.watchedMovies"
+          :key="movie.id"
+          :movie="movie"
+        />
+      </div>
+
       <h3>All Movies</h3>
       <Movie
         v-for="movie of movieStore.movies"
@@ -12,6 +39,8 @@
         :movie="movie"
       />
     </div>
+
+    <div class="search" v-else>Search</div>
   </div>
 </template>
 
@@ -48,14 +77,15 @@ const movieStore = useMovieStore()
   border-radius: 10px;
   cursor: pointer;
   background: #efefef;
-}
-.btn:hover {
-  opacity: 0.7;
-}
-.btn_green {
-  background: #37df5c;
-}
 
+  &:hover {
+    opacity: 0.7;
+  }
+
+  &__green {
+    background: #37df5c;
+  }
+}
 .tabs {
   display: flex;
   justify-content: center;
