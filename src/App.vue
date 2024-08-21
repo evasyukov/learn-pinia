cec16e635694dbec99b4a6e0f04ddfb19acc583e
evasyukov <egor.vasyukov1@gmail.com>
2024-08-21 14:08:30 +0700

    добавлен список фильмов

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,64 @@
 <template>
-
+  <div class="app">
+    <div class="header">
+      <img class="header-logo" src="" alt="" />
+      <h2>My Favorite Movies</h2>
+    </div>
+    <div class="movies">
+      <h3>All Movies</h3>
+      <Movie
+        v-for="movie of movieStore.movies"
+        :key="movie.id"
+        :movie="movie"
+      />
+    </div>
+  </div>
 </template>
 
-<script>
+<script setup>
+import { useMovieStore } from "./stores/MovieStore"
+import Movie from "./components/Movie.vue"
 
+const movieStore = useMovieStore()
 </script>
 
 <style lang="scss">
+.app {
+  max-width: 600px;
+  margin: auto;
+}
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
 
+  &-logo {
+    max-width: 50px;
+    margin-right: 10px;
+  }
+}
+
+.btn {
+  border: none;
+  width: 100px;
+  height: 40px;
+  font-size: 14px;
+  margin: 0 10px;
+  border-radius: 10px;
+  cursor: pointer;
+  background: #efefef;
+}
+.btn:hover {
+  opacity: 0.7;
+}
+.btn_green {
+  background: #37df5c;
+}
+
+.tabs {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createPinia } from "pinia"
+import { createApp } from "vue"
 
-createApp(App).mount('#app')
+import App from "./App.vue"
+
+import "./style.scss"
+
+createApp(App).use(createPinia()).mount("#app")
